a4526bd4994b342981ee40bf507880d11d216fc9
Minor <Davin.Minor@stlcop.local>
2017-10-30 18:20:44 -0500

    number 3

--- Number3.R
+++ Number3.R
@@ -6,3 +6,18 @@ dat <- read.csv(url("http://esapubs.org/archive/ecol/E084/093/Mammal_lifehistori
                 sep = "\t", nrows = 1440, na.strings = c("-999.00", "999.00"))
 
 #rest of your code goes here
+
+ggplot(data = dat, aes( x = mass.g., y = newborn.g.)) + 
+  labs(x= "Adult Mass", y= "Newborn Mass") + geom_point()
+
+ggplot(data = dat, aes (x = mass.g., y = newborn.g.)) + labs( x = "Adult Mass", y = "Newborn Rate") +
+  scale_x_log10() + scale_y_log10() + geom_point()
+
+ggplot(data = dat, aes (x = mass.g., y = newborn.g., color = order)) + labs( x = "Adult Mass", y = "Newborn Rate") +
+  scale_x_log10() + scale_y_log10() + geom_point()
+
+ggplot(data = dat, aes (x = mass.g., y = newborn.g.)) + labs( x = "Adult Mass", y = "Newborn Rate") +
+  scale_x_log10() + scale_y_log10() + facet_wrap(~order) + geom_point()
+
+ggplot(data = dat, aes (x = mass.g., y = newborn.g.)) + labs( x = "Adult Mass", y = "Newborn Rate") +
+  scale_x_log10() + scale_y_log10() + facet_wrap(~order) + geom_point() + geom_smooth(method = lm) 

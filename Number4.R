a44a6a41f2f449875eb34b1ce4c2a80c84dfaabe
Minor <Davin.Minor@stlcop.local>
2017-10-30 20:11:44 -0500

    number 4

--- Number4.R
+++ Number4.R
@@ -6,3 +6,19 @@ dat <- read.csv(url("http://www.esapubs.org/archive/ecol/E088/096/avian_ssd_jan0
                 sep = "\t", na.strings = c("-999.00", "999.00", "-999", "999"))
 
 #your code goes here
+ggplot(data= dat , aes (x= F_mass)) + labs( x = "Female Mass (g)") +
+  geom_histogram()
+
+ggplot(data= dat , aes (x= F_mass)) + labs( x = "Female Mass (g)") +
+  geom_histogram(fill = "blue") + scale_x_log10()
+
+ggplot(data = dat, aes(x = F_mass)) + labs (x = "Mass (g)") + geom_histogram(fill="blue") + 
+  geom_histogram(aes(x=M_mass, alpha = 0.3)) + scale_x_log10()
+
+ggplot(data = dat, aes(x = F_mass)) + geom_histogram(fill = "blue", alpha = 0.3) + labs(x = "mass (g)") +
+  geom_histogram(aes(x = M_mass), alpha = 0.3) + scale_x_log10() + facet_wrap(~ Family)
+
+ggplot(data = dat, aes(x = F_wing)) + geom_histogram(fill = "blue", alpha = 0.3) + labs(x = "Wing Size") +
+  geom_histogram(aes(x = M_wing), alpha = 0.3) + scale_x_log10() + facet_wrap(~ Family)
+
+
